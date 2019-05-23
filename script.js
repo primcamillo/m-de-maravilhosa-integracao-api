948a317eb4760d73a3b4fb77f0313932b723a881
primcamillo <pri.m.camillo@gmail.com>
2019-05-22 22:05:20 -0300

    adicionamos o nosso servidor e Formulario no index

--- script.js
+++ script.js
@@ -1,79 +1,51 @@
-//const app = document.getElementById('root');
-const authors = document.createElement('div');
-authors.setAttribute('class', 'authors');
-//app.appendChild(authors);
+const box = document.querySelector(".maravilhosas__box");
 
-fetch('https://theblackwomanhistory.firebaseio.com/.json')
-.then(function(response){
-    return response.json()
-})
-.then(function(data){const box = document.querySelector(".maravilhosas__box");
+//fetch('https://theblackwomanhistory.firebaseio.com/.json') para usar uma API ja feita
+fetch('http://localhost:5001/maravilhosas/') //API que fizemos
 
-
-fetch('https://theblackwomanhistory.firebaseio.com/.json')
-
-.then((response) =>{
-    return response.json();
-})
-.then((data) => {
-    data.content.forEach(mulher =>{
-
-        let perfil = document.createElement('div');
-        perfil.setAttribute('class', 'maravilhosas__perfil');
-        box.appendChild(perfil);
-        
-        let link = document.createElement('a');
-        link.href = '#!';
-        perfil.appendChild(link);
-
-        let imagem = document.createElement('img');
-        imagem.setAttribute('class', 'img-responsive');
-        imagem.src = '#!';
-        link.appendChild(imagem);
-
-        if 
-        else
-
-        let nome = document.createElement('p');
-        nome.innerHTML = mulher.title;
-        link.appendChild(nome);
-
-        
+    .then((response) => {
+        return response.json();
     })
-})
-.catch((erro) => {
-    console.log(erro);
-})
-    console.log("SUCESSO!!")
-    console.log(data)
-    data.content.forEach(pessoas => {
-        const maravilhosas__box = document.createElement('div');
-        maravilhosas__box.setAttribute('class', 'card');
-        authors.appendChild(maravilhosas__box);
+    .then((data) => {
 
-        const maravilhosas__perfil = document.createElement('img');
-        maravilhosas__perfil.setAttribute("src", pessoas.image);
-        maravilhosas__box.appendChild(maravilhosas__perfil);
+        data.content.forEach(mulher => {
 
-        /* const name = document.createElement('h2');
-        name.textContent = pessoas.name.first + " " + pessoas.name.last;
-        card.appendChild(name);
+            let perfil = document.createElement('div');
+            perfil.setAttribute('class', 'maravilhosas__perfil');
+            box.appendChild(perfil);
 
-        const local = document.createElement('h3');
-        local.textContent = pessoas.location.city + ", " + pessoas.nat;
-        card.appendChild(local);
+            let link = document.createElement('a');
+            link.href = '#!';
+            perfil.appendChild(link);
 
-        const email = document.createElement('p');
-        email.textContent = pessoas.email;
-        card.appendChild(email);
+            let imagem = document.createElement('img');
+            imagem.setAttribute('class', 'img-responsive');
+            imagem.src = '#!';
 
-        const phone = document.createElement ('p');
-        phone.textContent = pessoas.cell;
-        card.appendChild(phone);  */
 
-    });
-    
-})
-.catch(function(erro){
-    console.log(erro)
-})
+            if (mulher.metadata) {
+                if (mulher.metadata.image) {
+                    if (mulher.metadata.image.url) {
+                        imagem.setAttribute('src', mulher.metadata.image.url);
+                    }
+                } 
+                else {
+                    imagem.setAttribute('src', './img/img-mulher.png');
+                }
+            }
+            else{
+                imagem.setAttribute('src', './img/img-mulher.png');
+            }
+            
+            link.appendChild(imagem);
+
+            let nome = document.createElement('p');
+            nome.innerHTML = mulher.title;
+            link.appendChild(nome);
+
+
+        })
+    })
+    .catch((erro) => {
+        console.log(erro);
+    })
