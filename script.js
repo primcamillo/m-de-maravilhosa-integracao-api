28f3a9b3249fdde9e9bb91aafce0a3093b5485e5
primcamillo <pri.m.camillo@gmail.com>
2019-05-23 22:05:51 -0300

    Adicionado o delete

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const nome = document.querySelector('.nome')
+const img = document.querySelector('.img')
 const box = document.querySelector(".maravilhosas__box");
+
 
 //fetch('https://theblackwomanhistory.firebaseio.com/.json') para usar uma API ja feita
 fetch('http://localhost:5001/maravilhosas/') //API que fizemos
@@ -28,15 +31,18 @@ fetch('http://localhost:5001/maravilhosas/') //API que fizemos
                     if (mulher.metadata.image.url) {
                         imagem.setAttribute('src', mulher.metadata.image.url);
                     }
-                } 
+                }
                 else {
                     imagem.setAttribute('src', './img/img-mulher.png');
                 }
+            } else if (mulher.image) {
+                imagem.setAttribute('src', mulher.image)
             }
-            else{
+
+            else {
                 imagem.setAttribute('src', './img/img-mulher.png');
             }
-            
+
             link.appendChild(imagem);
 
             let nome = document.createElement('p');
@@ -49,3 +55,57 @@ fetch('http://localhost:5001/maravilhosas/') //API que fizemos
     .catch((erro) => {
         console.log(erro);
     })
+
+const button = document.querySelector('.button');
+button.addEventListener('click', () => {  //no meio dos parenteses era eventoq1
+    // evento.preventDefault();
+
+
+    fetch('http://localhost:5001/maravilhosas/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'title': nome.value,
+            'image': img.value,
+        })
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+        })
+
+        .catch((erro) => {
+            console.log('erro')
+        })
+})
+
+const bdeletar = document.querySelector('.deletar');
+bdeletar.addEventListener('click', () => {
+
+    fetch('http://localhost:5001/maravilhosas/', {
+        method: 'DELETE ',
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "id": deletar.getAttribute("data-id")
+
+        })
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then(() =>{
+            cardPai.removeChild(thisCard)
+        })
+
+        .catch((erro) => {
+            console.log('erro')
+        })
+         
+})
